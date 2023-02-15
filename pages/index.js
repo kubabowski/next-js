65716e350e6fbf6b4b701bb82868a6de14f8394c
szymon <szymon.jagielski@olicom.pl>
2023-02-15 07:33:04 +0100

    Eslint + prettier

--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,15 @@ import Hero from '../components/Hero'
 
 
 const Home = () => {
-  const mainHeroText = "Kalina Opalińska";
+  const mainHeroText = 'Kalina Opalińska';
 
   return (
     <div className=' min-h-screen flex items-center justify-center'>
       
       
       <Hero text={mainHeroText} />
-      
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
-
+export default Home;
