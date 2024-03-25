eb6a74efc3464139ef8b7451836208d4f8c86c65
kubabowski <99883972+kubabowski@users.noreply.github.com>
2024-03-25 23:47:38 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ const Home = () => {
     <div className=' min-h-screen flex items-center justify-center'>
       
       <div className='z-[1] mx-auto my-auto text-white flex flex-col items-center'>
-        <Link href='/o-mnie'><span className='text-xl md:text-4xl font-thin fira-extra'>KALINA OPALIŃSKA</span></Link>
+        <Link href='/o-mnie'><span className='text-xl md:text-4xl font-thin fira-extra ht-23'>KALINA OPALIŃSKA</span></Link>
         <div className="white-line" />
         <span className="poiret text-md md:text-lg">BEAUTY EXPERT - MAKEUP ARTIST - HAIR STYLIST</span>
       </div>
